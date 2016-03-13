93546eaacf1fb70625bd846862a95d548c6f3195
Peter Chim <pchim24@gmail.com>
2016-03-12 23:10:27 -0800

    Implement string parse and json substring helper functions

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -6,33 +6,58 @@ var parseJSON = function(json) {
   // your code goes here
   var i = 0;
 
+  // helper function to get the substring from i to end of json
+  var subJ = function(index){
+  	return json.substring(index,json.length);
+  }
+
   // function to parse array elements
   var parseArray = function(arrString){
   	var arr = [];
   	return arr;
   };
 
+  // function to parse object elements
   var parseObject = function(objString){
   	var obj = {};
   	return obj;
   };
 
+  // function to parse strings
+  var parseString = function(strString){
+  	var str = '';
+  	while(json[i] !== '\"' && i<json.length){
+  		str+=json[i];
+  		i++;
+  	}
+  	if (json.charAt(i) !== '\"'){
+  		throw(SyntaxError);
+  	}
+  	i++;
+  	return str;
+  };
+
+
   while(i < json.length){
   	console.log(json.length);
   	if (json[i] === '['){	// start an array
   		i++;
-  		var arr = parseArray(json.substring(i,json.length));
+  		var arr = parseArray(subJ(i));
   		if (json[i] === ']'){	// if the array closes, return it
   			i++;
   			return arr;
   		}
   	} else if (json[i] === '{'){
   		i++;
-  		var obj = parseObject(json.substring(i,json.length));
+  		var obj = parseObject(subJ(i));
   		if (json[i] === '}'){
   			i++;
   			return obj;
   		}
+  	} else if (json[i] === '\"'){
+  		i++;
+  		var str = parseString(subJ(i));
+  		return str;
   	}
   	i++;	// reach end of json
   }
