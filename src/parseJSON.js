5d5777ddd0600ec1d5e3800aa70cbad1943e8865
Peter Chim <pchim24@gmail.com>
2016-03-12 22:39:15 -0800

    Parse empty arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,25 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  var i = 0;
+
+  // function to parse array elements
+  var parseArray = function(arr){
+  	var arrayElements = [];
+  	return arrayElements;
+  }
+
+  while(i < json.length){
+  	console.log(json.length);
+  	if (json[i] === '['){	// start an array
+  		i++;
+  		var arr = parseArray(json.substring(i,json.length));
+  		if (json[i] === ']'){	// if the array closes, return it
+  			i++;
+  			return arr;
+  		}
+  	}
+  	i++;	// reach end of json
+  }
+
 };
